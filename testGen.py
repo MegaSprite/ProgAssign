9547db15a2352d7b3140a83b6111129295b19dfc
MegaSprite <k12dyc@qq.com>
2018-01-05 15:49:14 -0500

    doesn't work lol

--- testGen.py
+++ testGen.py
@@ -1,3 +1,14 @@
 #! ~/Rob550/venv/bin/python
 
-print "Hello World"
+import numpy as np
+import sys
+
+filename, row, col = sys.argv
+if row.isdigit() == 0 or col.isdigit() == 0:
+    sys.stderr.write('ERROR: expected two integer arguments')
+else:
+    row = int(row)
+    col = int(col)
+    m = np.random.randn(row,col)
+    M = '\n'.join((','.join(str(a) for a in b)) for b in m)
+print M
